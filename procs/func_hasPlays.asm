29a12149f2b4827c1f4ded88404745db373ec0fd
Unknown <R_P_1997@hotmail.com>
2018-06-05 20:36:03 +0100

    HasPlays Proc (complete)

--- procs/func_hasPlays.asm
+++ procs/func_hasPlays.asm
@@ -1,70 +1,124 @@
+posCell_now		dw 0
+corCell_now		db 0
+max_linhas		db 6
+max_colunas 	db 9
+
+debug_table macro  ;Debug
+	mov byte ptr es:[bx-1],'Y'
+	call func_makeDelay
+	call func_makeDelay
+	call func_makeDelay
+	call func_makeDelay
+	call func_makeDelay
+	mov byte ptr es:[bx-1],' '
+endm
+
 func_hasPlays proc
 	start:
-		xor dx ;Contador de espaços possiveis
-		mov	al, 160;Espaços por linha
-		mov	ah, 9;POSy
-		mul	ah ;160 * POSy
-		add	ax, 62; (160 * POSy) + POSx
-		mov bx, ax; BX = AX
-		add bx, 1 ;Para obter a cor do primeiro bloco
-		mov cl, 6;Quantidade máxima de linhas a verificar
-		mov ch, 9;Quantidade máxima de colunas a verificar
+		xor dx, dx ;Contador de espaços possiveis
+		mov ax, 1341
+		mov posCell_now, ax
+		mov bx, ax;160 * 8 + 60 + 1 (Celulas por linha * linhas + Celulas até a posição em X + Valor para obter a cor (0 - caracter, 1 - cor)
+		mov cl, 1;Quantidade máxima de linhas a verificar
+		mov ch, 1;Quantidade máxima de colunas a verificar
 	get_color_around:
-		mov	al, es:[bx] ;Posição do cursor
+		xor dx, dx ;Contador de espaços possiveis
+		mov ax, posCell_now
+		mov bx, ax
+		mov	al, es:[bx] ;Cor na posição do cursor
+		mov corCell_now, al
+		mov byte ptr es:[bx-1],'X' ;Debug
+		;mov byte ptr es:[bx],7
+		
+		;espera_tecla
+		
 		linhas:
-			mov ch, 9
-			dec cl
-			cmp cl, 0
-			jl no_plays
 			top:
 				inc dl ;Quantidade de espaços com a cor igual à do cursor
 				sub bx, 160 ;Mudar para a linha em cima
 				mov ah, es:[bx] ;Posição em cima do cursor
+				
+				debug_table ;Debug
+				
+				mov al, corCell_now
 				cmp ah, al ;Verificar se a cor do bloco atual é igual ao do cursor
 				je top ;Se sim, repete
-				mov al, 160
-				mov ah, dl 
-				mul ah ;AX = AL * AH (AX = 160*Quant)
-				add bx, ax ; Volta à posição do cursor
+				dec dl ;Se não for igual, não incrementa a quantidade de espaços com a cor igual
+				mov ax, posCell_now
+				mov bx, ax
 			bottom:
-				inc dh ;Quantidade de espaços com a cor igual à do cursor
+				inc dl ;Quantidade de espaços com a cor igual à do cursor
 				add bx, 160 ;Mudar para a linha em baixo
 				mov ah, es:[bx] ;Posição em baixo do cursor
+				
+				debug_table ;Debug
+				
+				mov al, corCell_now
 				cmp ah, al ;Verificar se a cor do bloco atual é igual ao do cursor
 				je bottom ;Se sim, repete
-				add dl, dh ;DL = DL + DH
-				cmp dl, 3 ;Se DL >= 3, tem jogadas
+				dec dl ;Se não for igual, não incrementa a quantidade de espaços com a cor igual
+				cmp dl, 3	 ;Se DL >= 3, tem jogadas
 				jge has_plays
-				mov al, 160
-				mov ah, dl 
-				mul ah ;AX = AL * AH (AX = 160*Quant)
-				add bx, ax ; Volta à posição do cursor
+				mov ax, posCell_now
+				mov bx, ax
 		colunas:
-			dec ch
 			left:
-				inc dl ;Quantidade de espaços com a cor igual à do cursor
-				sub bx, 2 ;Mudar para o bloco atrás
+				inc dh ;Quantidade de espaços com a cor igual à do cursor
+				sub bx, 4 ;Mudar para o bloco atrás
 				mov ah, es:[bx] ;Posição atrás do cursor
+				
+				debug_table ;Debug
+				
+				mov al, corCell_now
 				cmp ah, al ;Verificar se a cor do bloco atual é igual ao do cursor
 				je left ;Se sim, repete
-				mov al, 2
-				mov ah, dl
-				mul ah ;AX = AL * AH (AX = 2*Quant)
-				add bx, ax ; Volta à posição do cursor
+				dec dh ;Se não for igual, não incrementa a quantidade de espaços com a cor igual
+				mov ax, posCell_now
+				mov bx, ax
 			right:
 				inc dh
-				add bx, 2 ;Mudar para o bloco à frente
+				add bx, 4 ;Mudar para o bloco à frente
 				mov ah, es:[bx] ;Posição à frente do cursor
+				
+				debug_table ;Debug
+				
+				mov al, corCell_now
 				cmp ah, al ;Verificar se a cor do bloco atual é igual ao do cursor
 				je right ;Se sim, repete
-				add dl, dh ;DL = DL + DH
-				cmp dl, 3;Se DL >= 3, tem jogadas
+				dec dh ;Se não for igual, não incrementa a quantidade de espaços com a cor igual
+				cmp dh, 3;Se DL >= 3, tem jogadas
 				jge has_plays
-				cmp ch, 0
-				jl linhas
-				no_plays
+				mov ax, posCell_now
+				mov bx, ax
+				
+				mov ax, posCell_now
+				mov bx, ax
+				mov byte ptr es:[bx-1],' ' ;Debug
+				
+				cmp ch, max_colunas
+				jl next_column
+				cmp ch, max_colunas
+				jge next_line
+	next_line:
+		inc cl
+		cmp cl, max_linhas
+		jg no_plays
+		mov ch, 1
+		mov ax, posCell_now
+		add ax, 160
+		sub ax, 32
+		mov posCell_now, ax
+		jmp get_color_around
+	next_column:
+		inc ch
+		mov ax, posCell_now
+		add ax, 4
+		mov posCell_now, ax
+		jmp get_color_around
 	has_plays:
-		ret
+		ret 
 	no_plays:
+		call func_drawTabuleiro
+		jmp start
 		ret
 func_hasPlays endp
